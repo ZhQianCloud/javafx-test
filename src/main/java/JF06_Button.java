859f9b07e55c44292bb3d448cbd320579e08b48a
zhqian <zhqiancloud@qq.com>
2022-06-12 00:07:06 +0800

    v6

--- src/main/java/JF06_Button.java
+++ src/main/java/JF06_Button.java
@@ -1,22 +1,118 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class JF06_Button extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Button button = new Button("按钮");
-        Scene scene = new Scene(button);
-        button.setPrefHeight(300);
-        button.setPrefWidth(200);
-        button.setLayoutY(200);
-        button.setLayoutX(200);
-
-        stage.setScene(scene);
         stage.setTitle("javafx");
         stage.setHeight(600);
         stage.setWidth(800);
+
+        Button button = new Button("按钮");
+        VBox vbox = new VBox();
+        Scene scene = new Scene(vbox);
+        stage.setScene(scene);
+
+        System.out.println("button = " + button.getText());
+        button.setPrefHeight(100);
+        button.setPrefWidth(200);
+        button.setLayoutY(10);
+        button.setLayoutX(10);
+        button.setFont(Font.font("Microsoft YaHei UI", FontWeight.BOLD, FontPosture.ITALIC,40));
+        //背景,圆角,边距
+        BackgroundFill backgroundFill = new BackgroundFill(Paint.valueOf("#FFB6C1"), new CornerRadii(10),new Insets(10));
+        Background background = new Background(backgroundFill);
+        button.setBackground(background);
+        //边缘样式
+        BorderStroke borderStroke = new BorderStroke(Paint.valueOf("#00FFFF"), BorderStrokeStyle.DOTTED,new CornerRadii(4),new BorderWidths(5));
+        Border border = new Border(borderStroke);
+        button.setBorder(border);
+        //鼠标样式
+        button.setCursor(Cursor.HAND);
+        //背景
+        button.setTextFill(Paint.valueOf("#F5F5F5"));
+        //类css样式
+        Button button2 = new Button("这是按钮2");
+        button2.setPrefHeight(50);
+        button2.setPrefWidth(100);
+        button2.setLayoutY(200);
+        button2.setLayoutX(20);
+        button2.setStyle(
+                "-fx-background-color: #1169EE20;"+
+                "-fx-text-fill: #FFFFFF;"+
+                "-fx-border-color: #F4A460;"+
+                "-fx-border-radius: 5;"+
+                "-fx-background-radius: 5;"+
+                "-fx-background-insets: 0;"+
+                "-fx-font-size: 15;"+
+                "-fx-font-family: 'Segoe Print';"+
+                "-fx-border-style: dotted;"+
+                "-fx-border-width: 4;"
+        );
+
+
+
+
+        vbox.getChildren().addAll(button,button2);
+
+        //单击事件
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Button but = (Button) actionEvent.getSource();
+                System.out.println("but.getText() = " + but.getText());
+            }
+        });
+
+        //双击时间
+        button2.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getClickCount()==2){
+                    System.out.println("button2被双击了");
+                }
+
+                if(mouseEvent.getButton().name().equals(MouseButton.PRIMARY.name())){
+                    System.out.println("button2:鼠标左键被点击了");
+                }
+                if(mouseEvent.getButton().name().equals(MouseButton.SECONDARY.name())){
+                    System.out.println("button2:鼠标右键被点击了");
+                }
+            }
+        });
+
+        //检测键盘
+        button2.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                System.out.println("setOnKeyPressed = " + keyEvent.getCode().getName());
+                if (keyEvent.getCode().getName().equals(KeyCode.A.getName())){
+                    System.out.println("指定的A被按下了");
+                }
+            }
+        });
+        button2.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                System.out.println("setOnKeyReleased = " + keyEvent.getCode().getName());
+            }
+        });
+
         stage.show();
     }
 }
